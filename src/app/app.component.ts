d3fef962eb858ab48fb88ec4322365bfba4d77ba
Rajendran R <rajendranr-5483@users.noreply.github.com>
2022-11-09 18:24:38 +0530

    File are modified based on WebApi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,18 @@ import { DialogComponent } from '@syncfusion/ej2-angular-popups';
 })
 export class AppComponent {
   title = 'file-manager-integration';
-  public hostUrl: string = 'https://ej2-aspcore-service.azurewebsites.net/';
+  public hostUrl: string = 'https://localhost:44352/';
   public ajaxSettings: object = {
-    url: this.hostUrl + 'api/FileManager/FileOperations'
+    url: this.hostUrl + 'api/FileManager/FileOperations',
+    getImageUrl: this.hostUrl + 'api/FileManager/GetImage',
+    uploadUrl: this.hostUrl + 'api/FileManager/Upload',
+    downloadUrl: this.hostUrl + 'api/FileManager/Download'
   };
+ 
   public service = 'https://ej2services.syncfusion.com/production/web-services/api/pdfviewer';
   public document = 'PDF_Succinctly.pdf';
   public docContainer: DocumentEditorContainerComponent | undefined;
   public dialog : DialogComponent | undefined;
-
-  public ngOnInit(): void {
-   this.dialog?.hide();
-  }
 
   fileOpen(args: FileOpenEventArgs) {
     const fileDetailsObj: any = args.fileDetails!;
@@ -31,8 +31,8 @@ export class AppComponent {
 
     if (fileType == '.pdf') {
       let responseText: any = this.getFileStream(filePath, true);
-      var pdfviewer = (<any>document.getElementById('pdfViewer')).ej2_instances[0];
-      pdfviewer.load(responseText, null);
+      // var pdfviewer = (<any>document.getElementById('pdfViewer')).ej2_instances[0];
+      // pdfviewer.load(responseText, null);
     }
     else if (fileType == '.docx') {
       // let responseText: any = this.getFileStream(filePath, false);
@@ -55,7 +55,16 @@ export class AppComponent {
     ajax.onreadystatechange = () => {
       if (ajax.readyState === 4) {
         if (ajax.status === 200 || ajax.status === 304) {
-          return ajax.responseText;
+          if (!isPDF) {
+            // open SFDT text in document editor
+           // this.container.documentEditor.open(ajax.responseText);
+          } else {
+            var pdfviewer = (<any>document.getElementById('pdfViewer'))
+              .ej2_instances[0];
+            pdfviewer.load(ajax.responseText, null);
+            //opens the file in pdf viewer
+            //this.pdfView.load(ajax.responseText, null);
+          }
         }
       }
       return null;
